58c9b05192499471dbafd0ad0c4a0ae5cbedfdb5
hirunagrad <hirunagrad@gmail.com>
2021-07-27 21:31:21 +0530

    add logout

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -107,4 +107,17 @@ router.post("/login", async(req,res) => {
 });
 
 
+router.get("/logout", (req,res) => {
+ 
+   
+        res.cookie("token", "", {
+            httpOnly: true,
+            expires: new Date(0)
+         }).send();
+
+   
+    
+});
+
+
 module.exports = router;
